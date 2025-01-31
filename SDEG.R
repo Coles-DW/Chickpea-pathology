e6402fe2bd46fbdb6f8a0278347a52fbbc219c8f
Donovin Coles <148016148+Coles-DW@users.noreply.github.com>
2025-02-01 10:34:55 +1100

    Add files via upload
    This code may be used to generate a plot of the proportion of significantly differentially expressed genes.

---
+++ SDEG.R
@@ -0,0 +1,12 @@
+#read data
+DEG <- read.csv("SDEG.csv")
+head(DEG)
+
+p <- ggplot(DEG, aes(x=HPI, y=DEG, fill=Regulation)) +
+  geom_bar(stat="identity", position=position_dodge())+
+  labs(x="\n Time after inoculation (h)", y = "Differentially expressed genes \n")+
+  scale_y_continuous(limits=c(0,10000))+
+  theme_minimal()  
+g <- p + scale_fill_grey() 
+g + theme(text = element_text(size = 16))
+
